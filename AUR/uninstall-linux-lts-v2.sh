7ef7c9fb9556719ed8d0949b7bc3a23514f0126b
Erik Dubois <erik.dubois@gmail.com>
2018-05-03 08:30:33 +0200

    working on virtualbox scripts

--- AUR/uninstall-linux-lts-v2.sh
+++ AUR/uninstall-linux-lts-v2.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-set -e
+#
 ##################################################################################################################
-# Author	:	Erik Dubois
-# Website	:	https://www.erikdubois.be
-# Website	:	https://www.arcolinux.info
-# Website	:	https://www.arcolinux.com
-# Website	:	https://www.arcolinuxd.com
-# Website	:	https://www.arcolinuxforum.com
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
 
-sudo pacman -S --noconfirm --needed virtualbox
-sudo pacman -S --needed virtualbox-host-dkms
-sudo pacman -S --noconfirm --needed linux-lts-headers
+sudo pacman -R linux-lts linux-lts-headers
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "################################################################"
