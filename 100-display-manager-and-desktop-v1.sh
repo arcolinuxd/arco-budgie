ef02634f82855b83f96aec482b7cb5bf60e1a5ad
Erik Dubois <erik.dubois@gmail.com>
2019-10-30 14:19:13 +0100

    budgie extras

--- 100-display-manager-and-desktop-v1.sh
+++ 100-display-manager-and-desktop-v1.sh
@@ -24,11 +24,11 @@ sudo pacman -Syyu --noconfirm
 #installing displaymanager or login manager
 sudo pacman -S --noconfirm --needed lightdm
 sudo pacman -S --noconfirm --needed arcolinux-lightdm-gtk-greeter arcolinux-lightdm-gtk-greeter-settings
-sudo pacman -S --noconfirm --needed arcolinux-wallpapers-git 
+sudo pacman -S --noconfirm --needed arcolinux-wallpapers-git
 #installing desktop environment
 sudo pacman -S gnome --noconfirm --needed
 #sh AUR-DS/install-budgie-desktop-git-v*.sh
-sudo pacman -S budgie-desktop --noconfirm --needed
+sudo pacman -S budgie-desktop budgie-extras --noconfirm --needed
 #enabling displaymanager or login manager
 sudo systemctl enable lightdm.service -f
 sudo systemctl set-default graphical.target
